480764c8df69ae33231191a187a7e14b8d6a36ba
Martin CORNU <martin.cornu43@gmail.com>
2019-07-31 19:22:35 +0200

    modif design and add messages button

---
+++ app/src/main/java/com/alcedo/marty/interphone/MessagesActivity.kt
@@ -0,0 +1,12 @@
+package com.alcedo.marty.interphone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messages)
+    }
+}
