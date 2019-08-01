8ffce462581ffa9fd44386a6e98e7aec7731632e
Martin CORNU <martin.cornu43@gmail.com>
2019-08-01 11:14:56 +0200

    replay message 1

--- app/src/main/java/com/alcedo/marty/interphone/MessagesActivity.kt
+++ app/src/main/java/com/alcedo/marty/interphone/MessagesActivity.kt
@@ -1,12 +1,39 @@
 package com.alcedo.marty.interphone
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.VideoView
+import com.ebanx.swipebtn.SwipeButton
 
 class MessagesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messages)
+
+        //video
+        val vid = findViewById(R.id.videoView2) as VideoView
+        var path = ""
+        var u = Uri.parse(path)
+        vid.setVideoURI(u)
+        vid.setVisibility(View.INVISIBLE)
+        vid.setOnCompletionListener {
+            vid.setVisibility(View.INVISIBLE)
+        }
+
+        //Buttons
+        val playMessageButton1 = findViewById(R.id.playMessageButton1) as Button
+        playMessageButton1.setOnClickListener {
+            playMessageButton1.setVisibility(View.INVISIBLE)
+            path = "android.resource://com.alcedo.marty.interphone/"+R.raw.fetedetrop1
+            u = Uri.parse(path)
+            vid.setVideoURI(u)
+            vid.setVisibility(View.VISIBLE)
+            vid.start()
+        }
+
     }
 }
